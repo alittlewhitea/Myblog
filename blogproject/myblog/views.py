742955bd995d51b0ddf6ed69dbe58196e84d7911
alittlewhitea <895031098@qq.com>
2017-08-04 16:08:23 +0800

    首页
    欢迎首页

---
+++ blogproject/myblog/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'myblog/index.html', context={
+                      'Myblog title': '我的博客首页', 
+                      'welcome': '欢迎访问我的博客首页'
+                  })
