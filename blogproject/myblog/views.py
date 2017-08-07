47438972c9c08f72eeccc0ae2c197484c403f6ec
alittlewhitea <895031098@qq.com>
2017-08-07 15:23:29 +0800

    首页视图
    首页视图

--- blogproject/myblog/views.py
+++ blogproject/myblog/views.py
@@ -1,8 +1,14 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.shortcuts import render
+
+# def index(request):
+#     return render(request, 'myblog/index.html', context={
+#                       'title': '我的博客首页', 
+#                       'welcome': '欢迎访问我的博客首页'
+#                   })
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    return render(request, 'myblog/index.html', context={
-                      'title': '我的博客首页', 
-                      'welcome': '欢迎访问我的博客首页'
-                  })
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'myblog/index.html', context={'post_list': post_list})
